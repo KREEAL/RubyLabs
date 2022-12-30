80b3f1e8c88ce0a97f5a956497c7907628a81f0a
Цветков Киллриал <k-real1337@mail.ru>
2022-12-30 13:02:48 +0300

    Completed odd_greater_3

--- LR1/Task_2/task2.rb
+++ LR1/Task_2/task2.rb
@@ -15,5 +15,17 @@ def sum_of_prime_derivatives(number)
   sum
 end
 
-puts sum_of_primes 10
+puts sum_of_prime_derivatives 10
+
+def odd_greater_3(number)
+  count = 0
+  while number > 0
+    ost = number % 10
+    count+=1 if ost%2 == 1 and ost >3
+    number /=10
+  end
+  count
+end
+
+puts odd_greater_3 123456789
 
