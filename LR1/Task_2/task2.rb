d498cdf1e373fbc998c85f28ff2882b626049d4e
Цветков Киллриал <k-real1337@mail.ru>
2022-12-30 13:35:20 +0300

    Переписал вторую функцию более универсально, чтобы было круто

--- LR1/Task_2/task2.rb
+++ LR1/Task_2/task2.rb
@@ -1,3 +1,4 @@
+#Определение простоты числа
 def prime?(number)
   (2..number / 2).each { |i|
     if number % i == 0
@@ -7,6 +8,7 @@ def prime?(number)
   true
 end
 
+#Нахождение суммы простых делителей
 def sum_of_prime_derivatives(number)
   sum = 0
   (2..number).each { |i|
@@ -17,15 +19,42 @@ end
 
 puts sum_of_prime_derivatives 10
 
-def odd_greater_3(number)
+
+#Нахождение количества четных/нечетных цифр, больше/меньше заданного числа. Параметры:
+# oddeven: любое число. Его четность - четность искомых цифр
+# greaterlower: если число положительное, то поиск будет работать как "Строго больше". Отрицательное наоборот
+def odd_even_greater_lower_digits_count (number,oddeven,greaterlower,digit)
   count = 0
+  gold_ost = oddeven%2
+  grlob = false
+  grlob = true if greaterlower >= 0
   while number > 0
     ost = number % 10
-    count+=1 if ost%2 == 1 and ost >3
+    if ost%2 == gold_ost and ((ost > digit and grlob) or (ost<digit and !grlob))
+      count+=1
+    end
     number /=10
   end
   count
 end
 
-puts odd_greater_3 123456789
+puts odd_even_greater_lower_digits_count 123456789,1, -5,7
 
+def sumcifr(number)
+  acc = 0
+  while number >0
+    acc += number %10
+    number/=10
+  end
+  acc
+end
+
+# #Найти произведение делителей числа, сумма цифр которых меньше суммы цифр исходного
+#
+# def appl_derivatives(number)
+#   appl = 1
+#   old_sumcifr = sumcifr number
+#   (1..number/2).each do |i|
+#     appl*=
+#   end
+# end
