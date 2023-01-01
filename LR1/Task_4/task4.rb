12547b86674b0d575a660257d29acbcd753c5d58
Цветков Киллриал <k-real1337@mail.ru>
2023-01-01 19:59:55 +0300

    Задачка 49

--- LR1/Task_4/task4.rb
+++ LR1/Task_4/task4.rb
@@ -1,3 +1,4 @@
+require 'set'
 #Дан целочисленный массив. Необходимо найти количество элементов, расположенных после последнего максимального.
 def task1(arr)
   arr.reverse!.find_index(arr.min)
@@ -32,10 +33,41 @@ def task37(arr,index=1,count=0)
   end
 end
 
-a = [1,7,3,4,-5,6,10,9]
+def prime?(number)
+  (2..Math.sqrt(number)).each { |i|
+    if number % i == 0
+      return false
+    end
+  }
+  true
+end
 
 
-p task37 a
+def get_prime_derivatives(num,m=1,der_arr=[])
+  if m<=num
+    if num%m ==0 && (prime? m)
+      der_arr.push(m)
+    end
+    get_prime_derivatives(num,m+1,der_arr)
+  else
+    der_arr
+  end
+end
+
+
+#1.49. Для введенного списка положительных чисел построить список всех положительных простых делителей элементов списка без повторений.
+def task49(arr,arr_der=[])
+  if arr.length == 0
+    arr_der.to_set.to_a.sort!
+  else
+    task49(arr.slice(1,arr.length-1),arr_der+get_prime_derivatives(arr[0]))
+  end
+end
+
+a = [25,36,12,17,49,39]
+
+
+p task49 a
 
 # while 0!=1
 #   p "1,13,25,37,39"
