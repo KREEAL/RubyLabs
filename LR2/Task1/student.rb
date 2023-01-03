84c45b2cea25e05fefa57736277d2850e6679ab1
Цветков Киллриал <k-real1337@mail.ru>
2023-01-03 14:21:14 +0300

    Есть настраиваемые необязательные параметры, и можно передать в форме хеша их

--- LR2/Task1/student.rb
+++ LR2/Task1/student.rb
@@ -1,38 +1,26 @@
 # frozen_string_literal: true
 
 class Student
-
-  def initialize (surname,name,dadname,telephone="+XXXXXXXXXXX",telegram="@example",mail="example@example.example",git="@example")
-    @surname = surname
-    @name = name
-    @dadname = dadname
-    @telephone = telephone
-    @telegram = telegram
-    @mail = mail
-    @git = git
-    @id = __id__
+  def initialize(surname,name,dadname,options)
+      @surname = surname
+      @name = name
+      @dadname = dadname
+      @telephone = options[:telephone]
+      @telegram = options[:telegram]
+      @mail = options[:mail]
+      @git = options[:git]
+      @id = __id__
   end
 
   attr_accessor :name, :surname, :dadname,:telephone,:telegram,:mail,:git,:id
 
-  #Я нашел как пишутся геттеры и сеттеры, не бейте пж за attr_accessor
-  # def name
-  #   @name
-  # end
-  #
-  # def name=(value)
-  #   @name = value
-  # end
-
-
   def to_s
     grand_string = "#{@surname} #{@name} #{@dadname}"
-    grand_string+= " #{@telephone}" if @telephone!="+XXXXXXXXXXX"
-    grand_string+= " tg:#{@telegram}" if @telegram!="@example"
-    grand_string+= " #{@mail}" if @mail!="example@example.example"
-    grand_string+= " git:#{@git}" if @git!= "@example"
+    grand_string += " #{@telephone}" unless @telephone.nil?
+    grand_string+= " tg:#{@telegram}" unless @telegram.nil?
+    grand_string+= " #{@mail}" unless @mail.nil?
+    grand_string+= " git:#{@git}" unless @git.nil?
     grand_string
   end
 end
 
-s1 = Student.new('a','b','c',"+79649265792","@RetardKreeal")
