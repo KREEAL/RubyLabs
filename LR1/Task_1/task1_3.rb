f44f48912482c4ef53fc563b6770cab41dd6bb2a
Цветков Киллриал <k-real1337@mail.ru>
2023-01-03 13:48:54 +0300

    На досуге подыскал код для руби-команд

--- LR1/Task_1/task1_3.rb
+++ LR1/Task_1/task1_3.rb
@@ -1,7 +1,10 @@
 
-ruby_command = gets
-puts "#{ruby_command}"
+puts "Введите команду Ruby: "
+ruby_command = gets.chomp
+system "ruby -e \"#{ruby_command}\""
 
+
+puts "Введите команду ОС: "
 # если при вводе написать TYPE C:\Users\kirya\RubymineProjects\RubyLabs\LR1\Task_1\task1_3.rb, то получим на экран содержимое файла. Прикольно, классно
 os_command = gets
   puts `#{os_command}`
