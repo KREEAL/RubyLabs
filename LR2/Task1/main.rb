3b4899835c73b3c4f76ec1380ef181f0c10c0547
Цветков Киллриал <k-real1337@mail.ru>
2023-02-04 21:14:17 +0300

    Валидация всех полей класса

--- LR2/Task1/main.rb
+++ LR2/Task1/main.rb
@@ -1,9 +1,24 @@
-require 'C:\Users\kirya\RubymineProjects\RubyLabs\LR2\Task1\student.rb'
+require_relative 'student'
 
 
 student1 = Student.new("Tsvetkov","Kirill","Alexandrovich",{telephone:"+79649265792"})
-student2 = Student.new("Ivanov","Ivan","Ivanovich",{telegram:"@VanyaPro",mail:"vanyavanyavanya@mail.ru"})
-student3 = Student.new("Matcumoto","Yukihiro","Viktorovich",{git:"@DadOfRuby",telegram:"@RubyDad" })
+student2 = Student.new("Ivanov","Ivan","Ivanovich",{telegram:"VanyaPro",mail:"vanyavanyavanya@mail.ru",telephone:"+7964926582"})
+student3 = Student.new("Matcumoto","Yukihiro","Viktorovich",{git:"DadOfRuby",telegram:"RubyDad" })
 puts student1
 puts student2
 puts student3
+
+def test_invalid_fields(options)
+  Student.new("Кинг","Мартин","Лютер",options)
+rescue ArgumentError => e
+  puts e.message
+end
+
+test_invalid_fields({ phone: '0000000' })
+test_invalid_fields({ phone: 'samsung' })
+test_invalid_fields({ phone: '+79649265792' })
+test_invalid_fields({ mail: 'tsvetikov@mail' })
+test_invalid_fields({ mail: 'tsvetikov@mail.ru' })
+test_invalid_fields({ telegram: 'Webdev3000' })
+test_invalid_fields({ telegram: 'Всем привет я тут новенький' })
+# student1.first_name="AnDrEw"
--- LR2/Task1/student.rb
+++ LR2/Task1/student.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Student
-  def initialize(surname,name,dadname,options)
-      self.surname = surname
-      self.name = name
+  def initialize(surname,first_name,dadname,options)
+      self.last_name = surname
+      self.first_name = first_name
       self.dadname = dadname
 
       self.telephone = options[:telephone]
@@ -13,26 +13,69 @@ class Student
       self.id = options[:id]
   end
 
-  attr_accessor :name, :surname, :dadname,:telegram,:mail,:git,:id
-  attr_reader :telephone
 
-  def self.telephone_number?(number)
+  attr_accessor :id
+  attr_reader :last_name, :first_name, :dadname, :phone, :telegram, :mail, :git
+
+  #Общая проверка на ФИО
+  def self.valid_name?(name)
+    name.match(/(^[А-Я][а-я]+$)|(^[A-Z][a-z]+$)/)
+  end
+  #Общая проверка на имена тг/гита и тд
+  def self.valid_profile_name?(profile_name)
+    profile_name.match(/^[a-zA-Z0-9_.]+$/)
+  end
+  def self.valid_telephone?(number)
     return true unless (number =~ /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/).nil?
     false
   end
 
+  def self.valid_mail?(email)
+    email.match(/^(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/)
+  end
+
+  def last_name=(new_last_name)
+    raise ArgumentError, "Invalid argument: last_name=#{new_last_name}" unless Student.valid_name?(new_last_name)
+    @last_name = new_last_name
+  end
+
+  def first_name=(new_first_name)
+    raise ArgumentError, "Invalid argument: first_name=#{new_first_name}" unless Student.valid_name?(new_first_name)
+    @first_name = new_first_name
+  end
+
+  def dadname=(new_dadname)
+    raise ArgumentError, "Invalid argument: dadname=#{new_dadname}" unless Student.valid_name?(new_dadname)
+    @father_name = new_dadname
+  end
+
   def telephone=(new_telephone)
-    raise ArgumentError, "Invalid argument: telephone=#{new_telephone}" unless new_telephone.nil? || Student.telephone_number?(new_telephone)
-    @telephone = new_phone
+    raise ArgumentError, "Invalid argument: telephone=#{new_telephone}" unless new_telephone.nil? || Student.valid_telephone?(new_telephone)
+    @telephone = new_telephone
+  end
+
+  def telegram=(new_telegram)
+    raise ArgumentError, "Invalid argument: telegram=#{new_telegram}" unless new_telegram.nil? || Student.valid_profile_name?(new_telegram)
+    @telegram = new_telegram
+  end
+
+  def git=(new_git)
+    raise ArgumentError, "Invalid argument: git=#{new_git}" unless new_git.nil? || Student.valid_profile_name?(new_git)
+    @git = new_git
+  end
+
+  def mail=(new_mail)
+    raise ArgumentError, "Invalid argument: email=#{new_mail}" unless new_mail.nil? || Student.valid_mail?(new_mail)
+    @email = new_mail
   end
 
 
   def to_s
     grand_string = "#{@surname} #{@name} #{@dadname}"
     grand_string += " #{@telephone}" unless @telephone.nil?
-    grand_string+= " tg:#{@telegram}" unless @telegram.nil?
-    grand_string+= " #{@mail}" unless @mail.nil?
-    grand_string+= " git:#{@git}" unless @git.nil?
+    grand_string += " tg:#{@telegram}" unless @telegram.nil?
+    grand_string += " #{@mail}" unless @mail.nil?
+    grand_string += " git:#{@git}" unless @git.nil?
     grand_string
   end
 end
