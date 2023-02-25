fab69ee263f95ca2b752cbab83dad8868834aa62
Цветков Киллриал <k-real1337@mail.ru>
2023-02-25 17:14:21 +0300

    Получается, дататейбл

---
+++ LR2/anti_diagram/data_table.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class DataTable
+  def initialize(table)
+    self.table = table
+  end
+
+  private
+  attr_accessor :table
+
+end
--- LR2/anti_diagram/main.rb
+++ LR2/anti_diagram/main.rb
@@ -1,26 +1,27 @@
 require_relative 'student'
 require_relative 'student_base'
 require_relative 'student_short'
+require_relative 'data_table'
 
 
-student1 = Student.new("Tsvetkov","Kirill","Alexandrovich",{telephone:"+79649265792"})
-student2 = Student.new("Ivanov","Ivan","Ivanovich",{telegram:"VanyaPro",mail:"vanyavanyavanya@mail.ru",telephone:"+7964926582"})
-student3 = Student.new("Matcumoto","Yukihiro","Viktorovich",{git:"DadOfRuby",telegram:"RubyDad" })
+# student1 = Student.new("Tsvetkov","Kirill","Alexandrovich",{telephone:"+79649265792"})
+# student2 = Student.new("Ivanov","Ivan","Ivanovich",{telegram:"VanyaPro",mail:"vanyavanyavanya@mail.ru",telephone:"+7964926582"})
+# student3 = Student.new("Matcumoto","Yukihiro","Viktorovich",{git:"DadOfRuby",telegram:"RubyDad" })
 
-
-studslist = Student.read_from_txt('C:\Users\kirya\RubymineProjects\RubyLabs\LR2\anti_diagram\students.txt')
-
-studslist.each do |stud|
-  puts stud.getInfo
-end
-
-great_students_list = [student1,student2,student3]
-file_path = 'C:\Users\kirya\RubymineProjects\RubyLabs\LR2\anti_diagram\students1.txt'
-Student.write_to_txt(great_students_list,file_path)
-great_students_list_parsed = Student.read_from_txt(file_path)
-great_students_list_parsed.each do |stud|
-  puts stud.getInfo
-end
+#
+# studslist = Student.read_from_txt('C:\Users\kirya\RubymineProjects\RubyLabs\LR2\anti_diagram\students.txt')
+#
+# studslist.each do |stud|
+#   puts stud.getInfo
+# end
+#
+# great_students_list = [student1,student2,student3]
+# file_path = 'C:\Users\kirya\RubymineProjects\RubyLabs\LR2\anti_diagram\students1.txt'
+# Student.write_to_txt(great_students_list,file_path)
+# great_students_list_parsed = Student.read_from_txt(file_path)
+# great_students_list_parsed.each do |stud|
+#   puts stud.getInfo
+# end
 
 # a = student2.to_json
 # student4 = Student.from_json(a)
