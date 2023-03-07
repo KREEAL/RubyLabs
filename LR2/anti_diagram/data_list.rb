dd560c9403b2449b29a45bafb4db6ed32d5bffcf
Цветков Киллриал <k-real1337@mail.ru>
2023-03-07 12:36:13 +0300

    Класс абстрактный. Добавлены методы работы с selected объектами. Добавлены два абстрактных метода.

--- LR2/anti_diagram/data_list.rb
+++ LR2/anti_diagram/data_list.rb
@@ -1,10 +1,40 @@
 class DataList
 
-  def initialize(list)
-    self.list = list
+  private_class_method :new
+
+
+  def initialize(objects)
+    self.objects_list = objects
+    self.selected_objects = []
   end
 
+  def select_by_number(number)
+    selected_objects.append(number)
+  end
+
+  def get_selected
+    selected_id_list = []
+    selected_objects.each { |num|
+      selected_id_list.append(objects_list[num].id)
+    }
+    selected_id_list
+  end
+
+  def clear_selected
+    self.objects_list = []
+  end
+
+  protected
+  def get_names
+    raise NotImplementedError, 'You should implement get_names when extend DataList'
+  end
+
+  def get_data
+    raise NotImplementedError, 'You should implement get_data when extend DataList'
+  end
+
+
   private
-  attr_accessor :list
+  attr_accessor :objects_list, :selected_objects
 
 end
