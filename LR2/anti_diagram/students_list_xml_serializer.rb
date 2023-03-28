803364dce2865bface8e755bca245f156e915b17
KREEAL <k-real1337@mail.ru>
2023-03-28 13:31:33 +0300

    Внедрение XML в уже существующую Стратегию

---
+++ LR2/anti_diagram/students_list_xml_serializer.rb
@@ -0,0 +1,14 @@
+require 'xmlsimple'
+
+class StudentsXmlSerializer < StudentsListSerializer
+  def serialize(data, filename)
+    xml = XmlSimple.xml_out(data.map(&:to_hash), root_name: 'students')
+    File.write(filename, xml)
+  end
+
+  def deserialize(filename)
+    xml = File.read(filename)
+    data = XmlSimple.xml_in(xml, force_array: false)
+    data['student'].map { |student| Student.from_hash(student) }
+  end
+end
