b32fd6ce8fedd111b9a60ac7703e5198b73d8d16
KREEAL <k-real1337@mail.ru>
2023-04-05 20:44:44 +0300

    Налажено подключение к sqlite + DDL/DML скрипты

---
+++ LR2/anti_diagram/db_files/DDL/create_table_student.sql
@@ -0,0 +1,10 @@
+CREATE TABLE student (
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         first_name TEXT,
+                         last_name TEXT,
+                         dadname TEXT,
+                         telephone TEXT,
+                         telegram TEXT,
+                         mail TEXT,
+                         git TEXT
+);
---
+++ LR2/anti_diagram/db_files/DML/fish_students_insert.sql
@@ -0,0 +1,6 @@
+insert into student (first_name, last_name, dadname, telephone, telegram, mail, git)
+values ('Поджог','Сараев','Керосинович','+79999265792','podjhogsar','podjog@mail.ru',NULL),
+       ('Загон','Баранов','Монголович','+79996579229',NULL,'zagon@mail.ru','b4r4nza'),
+       ('Карман','Пистонов','Пулемётович','+79624587672','positronpiston','piston@mail.ru','gitgitbanza'),
+       ('Рулон','Обоев','Фотоевич','+79621112344','rulonon','rulon@mail.ru','oboevrul0n'),
+       ('Виталий','Головий','Александрович','+79549265787','vitalijg','vitalokpotato@mail.ru','golovik1111')
